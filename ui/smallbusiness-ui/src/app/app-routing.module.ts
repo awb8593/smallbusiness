07807169635ac5dcb42d8048e1e37661090cef56
m-dorsey <dorseymakenzie@gmail.com>
2022-11-19 14:29:26 -0500

    working on routing to home page component

--- ui/smallbusiness-ui/src/app/app-routing.module.ts
+++ ui/smallbusiness-ui/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomePageComponent } from "./home-page/home-page.component";
+
+const routes: Routes = [
+  { path: 'home-page', component: HomePageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
